b95f48e5f06e0ebcb53efbb4994a0e619ba0f50e
Issac <ichaudr1@umbc.edu>
2021-04-08 00:21:42 -0400

    pseudocount value changed

--- src/testing/testing_motif_distances.py
+++ src/testing/testing_motif_distances.py
@@ -11,8 +11,8 @@ from soa_motif_analysis import  *
 from Bio.motifs import Motif, Instances
 
 
-test_meme_output_1 = '/Users/ichaudr/Documents/UMBC/Lab-Erill/Isaac/issac-workspace-IE/soa/meme_bin/CLSTR713_meme_out/'
-test_meme_output_2 = '/Users/ichaudr/Documents/UMBC/Lab-Erill/Isaac/issac-workspace-IE/soa/meme_bin/CLSTR1708_meme_out/'
+test_meme_output_1 = '/Users/ichaudr/Documents/UMBC/Lab-Erill/Isaac/issac-workspace-IE/soa/meme_bin/CLSTR1708_meme_out/'
+test_meme_output_2 = '/Users/ichaudr/Documents/UMBC/Lab-Erill/Isaac/issac-workspace-IE/soa/meme_bin/CLSTR771_meme_out/'
 
 records_1 = get_motifs(test_meme_output_1, 100)
 records_2 = get_motifs(test_meme_output_2, 100)
@@ -33,24 +33,33 @@ motif_b = records_2[0]
 #motif_a = Motif(instances=Instances(['ATAAGCCACTACACCCCATAA','ATAATCCACTACACCCCATAA']))
 #motif_b = Motif(instances=Instances(['ATAATCCACTACACCCCATAA']))
 
+'''
 print(motif_a.pwm)
 
-val = 2
+val = .000000001/len(motif_a.instances)
 test_dict = dict(A=val, C=val, G=val, T=val)
 
 motif_a.pseudocounts = test_dict
 
+print(motif_a.pwm)'''
+
 print(motif_a.pwm)
+print(motif_b.pwm)
 
-print(calculate_motif_distance(motif_a, motif_b, padded=True, distance_function=calc_kld_distance))
+print(calculate_motif_distance(motif_a, motif_b, padded=True, distance_function=calc_kld_distance, add_psuedocounts=False))
 
-val = 1/len(motif_b.instances)
+#val = .25
+val = (len(str(motif_b.instances).split('\n')[0]) + len(str(motif_a.instances).split('\n')[0]) / 2)/(len(motif_b.instances) + len(motif_a.instances) / 2)
 test_dict = dict(A=val, C=val, G=val, T=val)
 
 motif_a.pseudocounts = test_dict
 motif_b.pseudocounts = test_dict
 
-print(calculate_motif_distance(motif_a, motif_b, padded=True, distance_function=calc_kld_distance))
+print(calculate_motif_distance(motif_a, motif_b, padded=True, distance_function=calc_kld_distance, add_psuedocounts=False))
+
+
+print(motif_a.pwm)
+print(motif_b.pwm)
 
 '''
 print(motif_a.instances[0])
